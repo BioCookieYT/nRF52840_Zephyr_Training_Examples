b9642d3fab269467a6b2d363bb397bd786346408
BioKeks <63932294+BioCookieYT@users.noreply.github.com>
2025-10-22 14:19:42 +0200

    Add initial blinky Zephyr application
    Introduces a new 'blinky' sample project for Zephyr, including .gitignore, CMakeLists.txt, prj.conf, and main.c. The application toggles an LED using GPIO every second.

---
+++ blinky/src/main.c
@@ -0,0 +1,30 @@
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+
+#define LED0_NODE DT_ALIAS(led0)
+
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void main(void)
+{
+    int ret;
+
+    if (!device_is_ready(led.port)) {
+        return;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(1000);
+    }
+}
+
+
